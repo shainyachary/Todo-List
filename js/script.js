c21bf4a93e542bc230f1870c1367671db1ed9bbb
acharygirish <acharygirish@gmail.com>
2023-01-11 00:27:12 +0530

    modified index css js

--- js/script.js
+++ js/script.js
@@ -5,17 +5,15 @@ var taskList = document.getElementById('taskList');
 count = 0;
 document.querySelector('#addTask').addEventListener('click',()=>
 {
-    if(taskName.value == '')
+    if(taskName.value == '' || taskDate.value == '' || taskDesc.value == '')
     {
         alert('Please enter the task....');
     }
     else
     {
         count++;
-        taskList.innerHTML += '<li class="task" id="task'+count+'"><input type="checkbox" id="check'+count+'" onchange="changeStatus('+count+')"><div class="content"><span id="text'+count+'">'+taskName.value+'</span><span class="small text-secondary" id="date'+count+'">'+taskDate.value+'</span><span id="desc'+count+'">'+taskDesc.value+'</span></div><div class="icon"><i class="fas fa-trash" onclick="removeTask('+count+')"></i></div></li>';
-        taskName.value = '';
-        taskDate.value = '';
-        taskDesc.value = '';
+        taskList.innerHTML += '<li class="task" id="task'+count+'"><div class="content"><input type="checkbox" id="check'+count+'" onchange="changeStatus('+count+')"><span class="text" id="text'+count+'">'+taskName.value+'</span></div><span class="date" id="date'+count+'">'+taskDate.value+'</span><span class="desc" id="desc'+count+'">'+taskDesc.value+'</span><div class="icon"><i class="fas fa-trash" onclick="removeTask('+count+')"></i></div></li>';
+        taskName.value = taskDate.value = taskDesc.value = '';
     }
 });
 function changeStatus(id)
@@ -24,14 +22,12 @@ function changeStatus(id)
     var taskTitle = document.getElementById("text"+id);
     var taskDate = document.getElementById("date"+id);
     var desc = document.getElementById("desc"+id);
-    taskTitle.style.textDecoration = status ? 'line-through' : 'none';
-    taskDate.style.textDecoration = status ? 'line-through' : 'none';
-    desc.style.textDecoration = status ? 'line-through' : 'none';
+    var task = document.getElementById('task'+id);
+    taskTitle.style.textDecoration = taskDate.style.textDecoration = desc.style.textDecoration = status ? 'line-through' : 'none';
+    task.style.backgroundColor = status ? 'lightgreen':'#e2dbdb';
 }
 function removeTask(id)
 {
-    var flag = confirm("Are you want to delete this task ? ");
+    var flag = confirm("Are you sure want to delete this task ? ");
     flag ? document.getElementById("task"+id).remove(): " ";
 }
-
-// taskList.innerHTML += '<div class="task" id="task'+count+'"><input type="checkbox" id="check'+count+'" onchange="changeStatus('+count+')"> <span id="text'+count+'">'+taskName.value + '</span> <input type="button" onclick="removeDiv('+count+')" value="X" /></div><br>';
